bced59b1b8d09fabf3fc3cf2a408620893cf638c
mohamed-houcine <168686140+mohamed-houcine@users.noreply.github.com>
2024-05-01 23:10:39 +0100

    Add files via upload

---
+++ neurone.h
@@ -0,0 +1,18 @@
+#ifndef NEURAL_NETWORK_H_INCLUDED
+#define NEURAL_NETWORK_H_INCLUDED
+
+#include <stdio.h>
+#include <math.h>
+
+struct neuron {
+    double output;
+    double bias;
+    double weight;
+};
+typedef struct neuron neuron;
+
+struct layer {
+    int n;
+    neuron *t;
+};
+typedef struct layer layer;
