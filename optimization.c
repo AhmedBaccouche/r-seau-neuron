5464691ad07ea5f77362fc0a4000fa2100c6aef4
AhmedBaccouche <148688324+AhmedBaccouche@users.noreply.github.com>
2024-05-02 22:53:57 +0100

    Add files via upload

---
+++ optimization.c
@@ -0,0 +1,25 @@
+#include <math.h>
+
+double derivative_sigmoid(double a)
+{
+    return a*(1-a);
+}
+
+double derivative_loss(double y,double a)
+{
+    return (a-y)/(a*(1-a));
+}
+
+double derivative_logLoss(double y,double a,double x)
+{
+    return derivative_loss(y,a) * derivative_sigmoid(a) * x;
+}
+
+double vect(double* w,double* x)
+{
+    double vect = 0;
+    for (int i = 0;i<2;i++){
+        vect += *(x+i) * *(w+i);
+    }
+    return vect;
+}
---
+++ optimization.h
@@ -0,0 +1,4 @@
+double derivative_sigmoid(double a);
+double derivative_loss(double y,double a);
+double derivative_logLoss(double y,double a,double x);
+double vect(double* w,double* x);
