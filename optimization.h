9a368a4251c2d0945aaca21ad7430759ab78004c
Ghazi Meddeb <ghazimdb@gmail.com>
2024-05-04 09:47:21 +0100

    Update optimization.h

--- optimization.h
+++ optimization.h
@@ -1,4 +1,8 @@
+#ifndef OPTIMIZATION_H_
+#define OPTIMIZATION_H_
+
 double derivative_sigmoid(double a);
 double derivative_loss(double y,double a);
 double derivative_logLoss(double y,double a,double x);
 double vect(double* w,double* x);
+#endif
