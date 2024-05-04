416d78e62d11aa9333697297ef782aaaf4e75a8d
Ghazi Meddeb <ghazimdb@gmail.com>
2024-05-04 09:56:58 +0100

    Update backpropagation.h

--- backpropagation.h
+++ backpropagation.h
@@ -1,5 +1,5 @@
-#ifndef BACKPROPAGATION
-#define BACKPROPAGATION
+#ifndef BACKPROPAGATION_H_
+#define BACKPROPAGATION_H_
 
 void backward_propagation(layer *input_layer, layer *output_layer, double learning_rate, double expected_output);
 #endif
