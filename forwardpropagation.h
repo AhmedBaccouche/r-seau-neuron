6044c71a69ec29ab7fbf2307815d3070b72c5a57
Ghazi Meddeb <ghazimdb@gmail.com>
2024-05-04 10:06:25 +0100

    Update forwardpropagation.h

--- forwardpropagation.h
+++ forwardpropagation.h
@@ -1,5 +1,5 @@
 #ifndef FORWARDPROPAGATION_H_
 #define FORWARDPROPAGATION_H_
 
-void forward_propagation(layer *input_layer, layer *output_layer);
+void forward_propagation(layer *, layer *);
 #endif
