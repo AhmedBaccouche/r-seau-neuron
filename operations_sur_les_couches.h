64db8f82d43dbab676970c34c816a770ff8a522f
Ghazi Meddeb <ghazimdb@gmail.com>
2024-05-04 10:07:37 +0100

    Update operations_sur_les_couches.h

--- operations_sur_les_couches.h
+++ operations_sur_les_couches.h
@@ -1,5 +1,5 @@
 #ifndef OPERATIONS_SUR_LES_COUCHES_H_
 #define OPERATIONS_SUR_LES_COUCHES_H_
 
-layer *creer_couche(int nombre_neuronnes);
+layer *creer_couche(int);  //créer une couche avec le nombre de neuronnes en entrée
 #endif
