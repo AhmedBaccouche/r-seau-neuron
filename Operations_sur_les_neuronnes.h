b352fdef8639442bffb192e6a46126f0543daa8a
Ghazi Meddeb <ghazimdb@gmail.com>
2024-05-04 09:52:06 +0100

    Update Operations_sur_les_neuronnes.h

--- Operations_sur_les_neuronnes.h
+++ Operations_sur_les_neuronnes.h
@@ -1,2 +1,6 @@
+#ifndef OPERATIONS_SUR_LES_NEURONNES_H_
+#define OPERATIONS_SUR_LES_NEURONNES_H_
+
 void initialiser_neuronne(neuronne *neuron);
 void unitary_output(neuronne *neuron,double input[2]);
+#endif
