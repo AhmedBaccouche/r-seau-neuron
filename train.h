2ed24b05c8ee4cd694e3776f48b189cc1e68e79e
Ghazi Meddeb <ghazimdb@gmail.com>
2024-05-04 10:40:57 +0100

    Update train.h

--- train.h
+++ train.h
@@ -1,5 +1,5 @@
 #ifndef TRAIN_H_
 #define  TRAIN_H_
 
-void train_network(layer *input_layer, layer *output_layer, double learning_rate, int epochs, double expected_output);
+void train_network(layer *, layer *, double, int, double);
 #endif
