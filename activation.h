732698f4fe87ab391f97d65de0a8378210437d09
Ghazi Meddeb <ghazimdb@gmail.com>
2024-05-04 10:05:10 +0100

    Update activation.h

--- activation.h
+++ activation.h
@@ -1,8 +1,8 @@
 #ifndef ACTIVATION_H_
 #define ACTIVATION_H_
 
-double sigmoid(double x);
-double Relu(double x);
-double tanh (double x);
-double hard_tanh (double x);
+double sigmoid(double);
+double Relu(double);
+double tanh (double);
+double hard_tanh (double);
 #endif
