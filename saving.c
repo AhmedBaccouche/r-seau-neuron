1b01bf333ca1c4a76c561d18b2a202f0fbf710d7
rayenbouajila <72107955+rayenbouajila@users.noreply.github.com>
2024-05-10 20:10:11 +0100

    Add files via upload

---
+++ saving.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void saveWeightBiasLoss(double weight[],double bias,double loss){
+    FILE*fp;
+    fp=fopen("weightsbiasloss.txt","a");
+    fprintf(fp,"weights=%lf,%lf-bias=%lf-loss=%lf\n",weight[0],weight[1],bias,loss) ;
+    fclose(fp);
+}
+int main(){
+    double weight[]={1,2};
+    saveWeightBiasLoss(weight,2.2,3.2);
+    return 0;
+}
---
+++ saving.h
@@ -0,0 +1,6 @@
+#ifndef SAVINGWBL_H_
+#define SAVINGWBL_H_
+#include<stdio.h>
+#include<stdlib.h>
+void saveWeightBiasLoss(double**,double,double);
+#endif
