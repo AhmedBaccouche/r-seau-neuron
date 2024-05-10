642e7ee5c03cb35abb7b18c5868db0ea1a408e3f
AhmedBaccouche <148688324+AhmedBaccouche@users.noreply.github.com>
2024-05-10 22:47:30 +0100

    Update saving.c

--- saving.c
+++ saving.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+#define COLS 3
+#define LINE 100
 
 
 void saveWeightBiasLoss(double weight[],double bias,double loss){
@@ -17,6 +18,44 @@ int main(){
     return 0;
 }
 
+
+
+
+
+void read_matrix(char *filename, double (*T)[COLS])
+{
+    FILE *fp;
+    fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
+        printf("Erreur lors de l'ouverture du fichier\n");
+        return;
+    }
+
+    char line[1024]; // Assuming a maximum line length of 1024 characters
+    int row = 0;
+
+    // Read each line from the file
+    while (fgets(line, sizeof(line), fp) && row < LINE)
+    {
+        char *token;
+        int col = 0;
+
+        // Parse the line using strtok
+        token = strtok(line, ";");
+        double value =  strtod(token, NULL); // Convert token to double
+        T[row][col++] = value; // Store in the matrix
+        token = strtok(NULL, ";");
+        value =  strtod(token, NULL); // Convert token to double
+        T[row][col++] = value; // Store in the matrix
+        token = strtok(NULL, ";");
+        value =  strtod(token, NULL);// Convert token to double
+        T[row][col++] = value; // Store in the matrix
+
+        row++; // Move to the next row
+    }
+    fclose(fp);
+}
 
 
 
