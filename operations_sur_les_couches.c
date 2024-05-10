1dd4d151596e9887ff196105638fbf76f687974f
AhmedBaccouche <148688324+AhmedBaccouche@users.noreply.github.com>
2024-05-10 20:10:58 +0100

    Update operations_sur_les_couches.c

--- operations_sur_les_couches.c
+++ operations_sur_les_couches.c
@@ -4,6 +4,8 @@
 #include "structures.c"
 #include "operations_sur_les_couches.h"
 
+#define COLS 3
+#define LINE 100
 
 layer *creer_couche(int nombre_neuronnes)
 {
@@ -38,5 +40,40 @@ layer *creer_couche(int nombre_neuronnes)
         current_neuron=current_neuron->next;
         }
     }
+}
+
+
+
+
+void read_matrix(char *filename, double (*T)[LINE][COLS])
+{
+    FILE *fp;
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Erreur lors de l'ouverture du fichier\n");
+        return;
+    }
+
+    char line[1024]; // Assuming a maximum line length of 1024 characters
+    int row = 0;
+
+    // Read each line from the file
+    while (fgets(line, sizeof(line), fp)) {
+        char *token;
+        int col = 0;
+
+        // Parse the line using strtok
+        token = strtok(line, ",");
+        while (token != NULL && col < COLS) {
+
+            double a= atof(token); // Convert token to integer and store in the matrix
+            strcpy(T[row][col++],token);
+            token = strtok(NULL, ",");
+        }
+
+        row++; // Move to the next row
+    }
+
+    fclose(fp);
 }
 
