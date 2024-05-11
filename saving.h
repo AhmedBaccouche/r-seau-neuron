ffac729acded940fbf560e9cd8d435ef0fe60e75
AhmedBaccouche <148688324+AhmedBaccouche@users.noreply.github.com>
2024-05-11 03:20:39 +0100

    Update saving.h

--- saving.h
+++ saving.h
@@ -4,4 +4,10 @@
 #include<stdlib.h>
 void saveWeightBiasLoss(double**,double,double);
 void read_matrix(char *filename, double (*T)[COLS]);
+int trouver(char ch[],char *filename);
+int trouver_bias(char ch[],char *filename);
+int trouver_weight(char ch[],char *filename);
+void afficher(char *filename);
+void min(char *filename, double *minimum, char *ligne);
+void affiche_min(char *filename,char *var,double m);
 #endif
